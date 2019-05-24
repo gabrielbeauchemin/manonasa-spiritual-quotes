198e731a3ce1ab5bada054a8d329f00bd21f5a2e
Gabriel Beauchemin <gabrielbeauchemin@hotmail.fr>
2019-05-23 22:33:51 -0400

    finish basic menu component

---
+++ src/Layout.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import menuIcon from './icons/hamburgerMenu.svg';
+import aboutIcon from './icons/about.svg';
+import contactIcon from './icons/contact.svg';
+import downloadIcon from './icons/download.svg';
+
+
+class Layout extends React.Component {
+    render() {
+        return (
+            <div className="menu" onClick={this.handleClick}>
+                <img src={menuIcon} alt="" height= "100%" width= "100%"/>
+                <ul style={{ display: 'none' }}>
+                    <li><a href="./"><img src={aboutIcon} alt="" className="menuIcon" />About</a></li>
+                    <li><a href="./"><img src={contactIcon} alt="" className="menuIcon"/>Download all quotes</a></li>
+                    <li><a href="./"><img src={downloadIcon} alt="" className="menuIcon"/>Contact me</a></li>
+                </ul>
+            </div>
+                )
+            }
+
+    handleClick() {
+        let menu = document.querySelectorAll('.menu ul')[0];
+        //toggle menu apparence
+        if (menu.style.display === "none") {
+                menu.style.display = "block";
+                //begin animation
+                menu.style.marginTop = "0.5em";
+        } 
+        else {
+                    menu.style.display = "none";
+                    menu.style.marginTop = "0em";
+        }
+        
+    }
+}
+        
+export default Layout;
