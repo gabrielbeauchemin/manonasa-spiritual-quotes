5fb6a9f6687d15788cc059a06cb65c15d40f3d65
Gabriel Beauchemin <gabrielbeauchemin@hotmail.fr>
2019-06-04 15:55:32 -0400

    Finished basic layout except language toggle

--- src/Layout.js
+++ src/Layout.js
@@ -1,23 +1,37 @@
 import React from 'react';
+import logo from './logo.svg';
 import menuIcon from './icons/hamburgerMenu.svg';
 import aboutIcon from './icons/about.svg';
 import contactIcon from './icons/contact.svg';
 import downloadIcon from './icons/download.svg';
+import searchIcon from './icons/search.svg';
+import searchRandomIcon from './icons/searchRandom.svg';
 
 
 class Layout extends React.Component {
     render() {
         return (
-            <div className="menu" onClick={this.handleClick}>
-                <img src={menuIcon} alt="" height= "100%" width= "100%"/>
-                <ul style={{ display: 'none' }}>
-                    <li><a href="./"><img src={aboutIcon} alt="" className="menuIcon" />About</a></li>
-                    <li><a href="./"><img src={contactIcon} alt="" className="menuIcon"/>Download all quotes</a></li>
-                    <li><a href="./"><img src={downloadIcon} alt="" className="menuIcon"/>Contact me</a></li>
-                </ul>
+            <div className="layout">
+                <div className="menu" onClick={this.handleClick}>
+                    <img src={menuIcon} alt="" height= "100%" width= "100%"/>
+                    <ul style={{ display: 'none' }}>
+                        <li><a href="./"><img src={aboutIcon} alt="" className="menuIcon" />About</a></li>
+                        <li><a href="./"><img src={contactIcon} alt="" className="menuIcon"/>Download all quotes</a></li>
+                        <li><a href="./"><img src={downloadIcon} alt="" className="menuIcon"/>Contact me</a></li>
+                    </ul>
+                </div>
+                <div className="logo" onClick={this.handleClick}>
+                    <img src={logo} alt="" height= "100%" width= "100%"/>
+                </div>
+                <form className="searchBox" action="./">
+                    <input type="text" alt="" placeholder="Search Spiritual Quotes" name="search" />
+                    <input className="searchIcon" title="search" alt="search" type="image" src={searchIcon} />
+                    <input className="searchIcon" title="search randomly" alt="search randomly" type="image" src={searchRandomIcon} />
+                </form>
             </div>
-                )
-            }
+                
+            );
+        }
 
     handleClick() {
         let menu = document.querySelectorAll('.menu ul')[0];
