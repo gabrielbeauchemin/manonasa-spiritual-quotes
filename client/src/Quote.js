e2c2cce71a3aa8f3d8ca83905b5921a13ceda4e1
Gabriel Beauchemin <gabrielbeauchemin@hotmail.fr>
2020-02-01 19:43:01 -0500

    added snackbar "copied" when a quote is copied

--- client/src/Quote.js
+++ client/src/Quote.js
@@ -2,26 +2,27 @@ import React from 'react';
 import copyIcon from './icons/copy.svg';
 import showMoreIcon from './icons/showMore.svg';
 import Modal from 'react-modal';
+import Snackbar from './Snackbar'
 
 class Quote extends React.Component {
 
-    constructor(props)
-    {
+    constructor(props) {
         super(props);
         this.state = {
             modalIsOpen: false,
-            modalStyle : {
-                content : {
-                  top                   : '45%',
-                  left                  : '50%',
-                  right                 : '20%',
-                  bottom                : '0%',
-                  marginRight           : '-50%',
-                  transform             : 'translate(-50%, -50%)'
+            modalStyle: {
+                content: {
+                    top: '45%',
+                    left: '50%',
+                    right: '20%',
+                    bottom: '0%',
+                    marginRight: '-50%',
+                    transform: 'translate(-50%, -50%)'
                 }
-              }
+            }
         };
 
+        this.snackbarRef = React.createRef();
         this.openModal = this.openModal.bind(this);
         this.closeModal = this.closeModal.bind(this);
     }
@@ -29,65 +30,71 @@ class Quote extends React.Component {
     render() {
         return (
             <div className="quote">
-                     {this.props.quote} <br/> <span style={{color: "gray"}}> -{this.props.author} </span>
-                     <br/>
-                     <br/>
-                     <div style={{backgroundColor: "#f4f6f9"}}>
-                        <span> 
-                            <button type="button" title="Copy quote to clipboard" className="quoteButton" onClick={((e) => this.copy( this.props.quote + " -" + this.props.author ))} >
+                {this.props.quote} <br /> <span style={{ color: "gray" }}> -{this.props.author} </span>
+                <br />
+                <br />
+                <div style={{ backgroundColor: "#f4f6f9" }}>
+                    <span>
+                        <button type="button" title="Copy" className="quoteButton" onClick={((e) => this.copyQuote(this.props.quote + " -" + this.props.author))} >
                             <img src={copyIcon} alt="" className="quoteIcon" />
-                            </button>
-                            <button type="button" title="See more information about the quote" className="quoteButton" onMouseDown={this.openModal}>
+                        </button>
+                        <Snackbar ref={this.snackbarRef} />
+                        <button type="button" title="See more information about the quote" className="quoteButton" onMouseDown={this.openModal}>
                             <Modal
                                 isOpen={this.state.modalIsOpen}
                                 onRequestClose={this.closeModal}
                                 style={this.state.modalStyle}
                                 closeTimeoutMS={250}
                             >
-                                <div onClick={this.closeModal} className="close"/> <br/>
-                                <h3 style={{margin: "0 auto", width: "40%"}}>Complete information about the quote</h3> <br/>
+                                <div onClick={this.closeModal} className="close" /> <br />
+                                <h3 style={{ margin: "0 auto", width: "40%" }}>Complete information about the quote</h3> <br />
                                 <div className="moreQuoteFields">
                                     <div className="moreQuoteField">
-                                        <b>Quote:&nbsp;</b> <p>{this.props.quote}</p> 
+                                        <b>Quote:&nbsp;</b> <p>{this.props.quote}</p>
                                     </div>
                                     <div className="moreQuoteField">
-                                        <b>Author:&nbsp;</b> <p>{this.props.author}</p> 
+                                        <b>Author:&nbsp;</b> <p>{this.props.author}</p>
                                     </div>
                                     <div className="moreQuoteField">
-                                        <b>Source:&nbsp;</b> <p>{this.props.source}</p> 
+                                        <b>Source:&nbsp;</b> <p>{this.props.source}</p>
                                     </div>
                                     <div className="moreQuoteField">
-                                        <b>Language:&nbsp;</b> <p>{this.props.language}</p> 
+                                        <b>Language:&nbsp;</b> <p>{this.props.language}</p>
                                     </div>
                                     {this.props.chapter !== undefined &&
                                         <div className="moreQuoteField">
-                                            <b>Chapter:&nbsp;</b> <p>{this.props.chapter}</p> 
+                                            <b>Chapter:&nbsp;</b> <p>{this.props.chapter}</p>
                                         </div>
                                     }
                                     {this.props.number !== undefined &&
                                         <div className="moreQuoteField">
-                                            <b>Number:&nbsp;</b> <p>{this.props.number}</p> 
+                                            <b>Number:&nbsp;</b> <p>{this.props.number}</p>
                                         </div>
                                     }
                                 </div>
                             </ Modal>
                             <img src={showMoreIcon} alt="" className="quoteIcon" />
-                            </button> 
-                        </span>
-                     </div>
+                        </button>
+                    </span>
+                </div>
             </div>
         );
     }
 
     //reference: https://techoverflow.net/2018/03/30/copying-strings-to-the-clipboard-using-pure-javascript/
-    copy(quote) {
+    copyQuote(quote) {
+        this.copyToClipBoard(quote);
+        this.snackbarRef.current.openSnackBar('Copied!');
+    }
+
+    copyToClipBoard(quote) {
         // Create new element
         var el = document.createElement('textarea');
         // Set value (string to be copied)
         el.value = quote;
         // Set non-editable to avoid focus and move outside of view
         el.setAttribute('readonly', '');
-        el.style = {position: 'absolute', left: '-9999px'};
+        el.style = { position: 'absolute', left: '-9999px' };
         document.body.appendChild(el);
         // Select text inside element
         el.select();
@@ -105,6 +112,6 @@ class Quote extends React.Component {
         this.setState({ modalIsOpen: false });
     }
 }
-        
-        
+
+
 export default Quote;
---
+++ client/src/Snackbar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+//Reference: https://medium.com/@farid12ansari7/make-a-snackbar-or-pop-up-notification-for-a-react-web-app-fd246dd1b9f0
+export class Snackbar extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isActive: false
+        };
+    }
+
+    openSnackBar = (message = 'Something went wrong...') => {
+        this.message = message;
+        this.setState({ isActive: true }, () => {
+            setTimeout(() => {
+                this.setState({ isActive: false });
+            }, 3000);
+        });
+    }
+
+    render() {
+        const { isActive } = this.state;
+        return (
+            <div className={isActive ? "snackbar snackbarShow" : "snackbar"}>
+                {this.message}
+            </div>
+        )
+    }
+}
+
+export default Snackbar;
