871458ab7b77246ab1c3909b3eb191bd829277a9
Gabriel Beauchemin <gabrielbeauchemin@hotmail.fr>
2020-04-22 23:24:08 -0400

    toggle visibility topNavMenu

--- client/src/TopNavMenu.js
+++ client/src/TopNavMenu.js
@@ -8,12 +8,12 @@ import downloadIcon from './icons/download.svg';
 const TopNavMenu = (props) => {
     const isMobile = useMediaQuery({
         query: '(max-device-width: 600px)'
-    })
+    });
     if (isMobile) {
         return (
-            <div className="menuMobile" onClick={() => menuClick(true)} tabIndex="1" onBlur={ () => menuBlur(true)} >
-                <img src={menuIcon} alt="" width="100%" height="100%" />
-                <ul style={{ display: 'none' }} >
+            <div className="menuMobile">
+                <img src={menuIcon} tabIndex={0} onClick={() => menuClick(true)} onBlur={(e) => menuBlur(e, true)} alt="" width="100%" height="100%" />
+                <ul style={{ display: 'none' }}  >
                     <li><a href="./about"><div style={{ display: "flex" }}><img src={aboutIcon} alt="" className="menuIcon" />About</div></a></li>
                     <li><a href="./download"><div style={{ display: "flex" }}><img src={contactIcon} alt="" className="menuIcon" />Download all quotes</div></a></li>
                     <li><a href="./contact"><div style={{ display: "flex" }}><img src={downloadIcon} alt="" className="menuIcon" />Contact</div></a></li>
@@ -22,9 +22,9 @@ const TopNavMenu = (props) => {
     }
     else {
         return (
-            <div className="menu" onClick={() => menuClick(false)} tabIndex="1" onBlur={() => menuBlur(false)} >
-                <img src={menuIcon} alt="" width="100%" height="100%" />
-                <ul style={{ display: 'none' }} >
+            <div className="menu" >
+                <img src={menuIcon} tabIndex={0} onClick={() => menuClick(false)} onBlur={(e) => menuBlur(e, false)} alt="" width="100%" height="100%" />
+                <ul style={{ display: 'none' }}   >
                     <li><a href="./about"><div style={{ display: "flex" }}><img src={aboutIcon} alt="" className="menuIcon" />About</div></a></li>
                     <li><a href="./download"><div style={{ display: "flex" }}><img src={contactIcon} alt="" className="menuIcon" />Download all quotes</div></a></li>
                     <li><a href="./contact"><div style={{ display: "flex" }}><img src={downloadIcon} alt="" className="menuIcon" />Contact</div></a></li>
@@ -34,27 +34,30 @@ const TopNavMenu = (props) => {
 }
 
 function menuClick(isMobile) {
-    let menuClass = isMobile ? '.menuMobile ul' : '.menu ul'; 
+    let menuClass = isMobile ? '.menuMobile ul' : '.menu ul';
     let menu = document.querySelectorAll(menuClass)[0];
     //toggle menu apparence
     if (menu.style.display === "none") {
         menu.style.display = "block";
-        //begin animation
-        menu.style.marginTop = "0.5em";
+        menu.style.marginTop = "10px";
     }
     else {
         menu.style.display = "none";
-        menu.style.marginTop = "0em";
+        menu.style.marginTop = "0px";
     }
 }
 
 function menuBlur(isMobile) {
-    let menuClass = isMobile ? '.menuMobile ul' : '.menu ul'; 
-    let menu = document.querySelectorAll(menuClass)[0];
-    menu.style.display = "none";
-    menu.style.marginTop = "0em";
-    document.activeElement.blur();
-
+    //hack: the blur is anywhere except the menu button...that means that the open menu also fires blur
+    //preventing the user to follow the hyperlink of a menu item
+    //a simple workaround is to fire it after 200ms, so if the user has clicked on a menu item
+    //he will be redirect to the requested page
+    setTimeout(function () {
+        let menuClass = isMobile ? '.menuMobile ul' : '.menu ul';
+        let menu = document.querySelectorAll(menuClass)[0];
+        menu.style.display = "none";
+        menu.style.marginTop = "0em";
+    }, 200);
 }
 
 export default TopNavMenu;
