a7ab9013f3c1e5860712ff20752e5ffa27b21ee4
Gabriel Beauchemin <gabybeauchemin@gmail.com>
2020-06-09 21:03:40 -0400

    changed mobile filters selection

--- client/src/ContentMobile.js
+++ client/src/ContentMobile.js
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import Filter from "./Filter";
 import QuoteMobile from "./QuoteMobile";
-import closeIcon from "./icons/close.svg";
+import backIcon from "./icons/back.svg";
 import plusIcon from "./icons/plus.svg";
 
 const ContentMobile = (props) => {
   const [contentMobileActivated, setContentMobileActivated] = useState(false);
-  const disableFilters = () => {
-    props.updateAuthorFilters(props.authorFilters);
-    props.updateSourceFilters(props.sourceFilters);
-    setContentMobileActivated(false);
-  };
+  const [nbrAuthorFiltersOn, setNbrAuthorFiltersOn] = useState(
+    props.authorFilters.length
+  );
+  const [nbrSourceFiltersOn, setNbrSourceFiltersOn] = useState(
+    props.sourceFilters.length
+  );
+  const nbrFiltersSelected = nbrAuthorFiltersOn + nbrSourceFiltersOn;
   return (
     <div>
       {!contentMobileActivated && (
         <div className="filtersLabelMobile">
-          <b>Filters</b>
+          <b>{props.language === "fr" ? "Filtres" : "Filters"}</b>
           <img
             className="toggleFiltersMobile"
             title="search"
@@ -26,6 +28,14 @@ const ContentMobile = (props) => {
             height="100%"
             onClick={() => setContentMobileActivated(true)}
           />
+          <b>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+            {props.language === "fr"
+              ? `(${nbrFiltersSelected} selected)`
+              : `(${nbrFiltersSelected} sélectionné${
+                  nbrFiltersSelected > 1 ? "s" : ""
+                })`}
+          </b>
         </div>
       )}
       <div className="contentMobile">
@@ -36,31 +46,37 @@ const ContentMobile = (props) => {
               title="search"
               alt="search"
               type="image"
-              src={closeIcon}
+              src={backIcon}
               width="100%"
               height="100%"
-              onClick={() => disableFilters()}
+              onClick={() => setContentMobileActivated(false)}
             />
           )}
-          {contentMobileActivated && (
-            <>
-              <Filter
-                display={props.language === "fr" ? "Auteur" : "Author"}
-                values={props.authorFilters}
-                onFilterChange={(authorFilters) =>
-                  props.updateAuthorFilters(authorFilters)
-                }
-              />
-              <Filter
-                display="Book"
-                values={props.sourceFilters}
-                selectedValues={props.sourceFiltersSelected}
-                onFilterChange={(sourceFilters) =>
-                  props.updateSourceFilters(sourceFilters)
-                }
-              />
-            </>
-          )}
+
+          <div
+            style={{
+              visibility: contentMobileActivated ? "visible" : "hidden",
+              display: contentMobileActivated ? "block" : "none",
+            }}
+          >
+            <Filter
+              display={props.language === "fr" ? "Auteur" : "Author"}
+              values={props.authorFilters}
+              onFilterChange={(authorFilters) => {
+                props.updateAuthorFilters(authorFilters);
+                setNbrAuthorFiltersOn(authorFilters.length);
+              }}
+            />
+            <Filter
+              display="Book"
+              values={props.sourceFilters}
+              selectedValues={props.sourceFiltersSelected}
+              onFilterChange={(sourceFilters) => {
+                props.updateSourceFilters(sourceFilters);
+                setNbrSourceFiltersOn(sourceFilters.length);
+              }}
+            />
+          </div>
         </div>
         <div className="quotes">
           {props.quotes.map((quote, index) => (
